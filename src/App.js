19d70a47897052abaafaed2ef90fec2a514e2a19
jimmurphyiv <jim.murphyiv@gmail.com>
2020-07-18 16:02:03 -0400

    layout complete working on media

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dropdownview: false
+    }
+  }
+
+  toggleDropdown = () => {
+    this.setState({dropdownVeiw: !this.state.dropdownview})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        
+        <div>
+          <header className="App-header">
+            <div className='Logo'>
+              Bootstrap
+            </div>
+              <div className='Nav'>
+                <li>About</li>
+                <li>Projects</li>
+                <li>Contact</li>
+              </div>
+              <div className='dropdown' onClick={this.toggleDropdown}>MENU</div>
+              { this.state.dropdownVeiw
+                ? (
+                  <div className='mobile-menu'>
+                  <li>About</li>
+                  <li>Projects</li>
+                  <li>Contact</li>
+                </div>
+                  
+                 ) :  null}
+          </header>
+        </div>
+            
+            <section>
+              <h1>GRAYSCALE</h1>
+              <p>A free, responsive, one page Bootstrap </p>
+              <p>theme created by Start Bootstrap.</p>
+              <button>GET STARTED</button>
+            </section>
+
+            
+      </div>
+    );
+  }
 }
 
 export default App;
